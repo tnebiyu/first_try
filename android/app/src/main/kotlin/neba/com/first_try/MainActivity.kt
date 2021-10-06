4acd0541f8ab22b81b557bfef94e25ceec75d464
nebahabesha <nebiyu28@gmail.com>
2021-10-06 13:32:55 +0300

    Initial commit

---
+++ android/app/src/main/kotlin/neba/com/first_try/MainActivity.kt
@@ -0,0 +1,6 @@
+package neba.com.first_try
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+//the main function is the starting point for all our flutter apps
+void main() {
+  runApp(
+    MaterialApp(
+      home: Center(
+        child: Text('Hello World'),
+      ),
+    ),
+  );
+}
