d71e3e89a622131e61110aa134ede8b7d81cc1e6
nebahabesha <nebiyu28@gmail.com>
2021-10-13 08:36:31 +0300

    changed tagged language from HTMLto Dart

--- lib/main.dart
+++ lib/main.dart
@@ -4,9 +4,18 @@ import 'package:flutter/material.dart';
 void main() {
   runApp(
     MaterialApp(
-      home: Center(
-        child: Text('Hello World'),
-      ),
+      home: Scaffold(
+          appBar: AppBar(
+            title: Text("I am Rich"),
+            backgroundColor: Colors.blueGrey,
+          ),
+          backgroundColor: Colors.white,
+          body: Center(
+            child: Image(
+              image: NetworkImage(
+                  'https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885__480.jpg'),
+            ),
+          )),
     ),
   );
 }
